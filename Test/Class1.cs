bc24dbf5aae6492a5572c811f55023c98aa150d4
abhib <rishibodake@gmail.com>
2020-07-18 14:27:43 +0530

    Test Case

---
+++ Test/Class1.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Class1
+    {
+        public Class1(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "email")]
+        [CacheLookup]
+        public IWebElement Email;
+
+
+        [FindsBy(How = How.Id, Using = "pass")]
+        [CacheLookup]
+        public IWebElement Password;
+
+
+        [FindsBy(How = How.Id, Using = "u_0_b")]
+        [CacheLookup]
+        public IWebElement Login;
+
+        public void Login1()
+        {
+            Email.SendKeys("9028809305");
+            Password.SendKeys("jocky1234#");
+            Login.Click();
+        }
+    }
+}
---
+++ Test/TestScena.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestScena
+    {
+        IWebDriver driver;
+        [OneTimeSetUp]
+        public void Init()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.facebook.com";
+        }
+
+        [Test]
+        public void Login()
+        {
+            Class1 page = new Class1(driver);
+            page.Login1();
+        }
+
+    }
+}
